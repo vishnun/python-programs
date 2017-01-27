f957bcb3e31bcd3b48f8b6cbafc259cd7f65c1dc
Vishnu N <vishnunarang2000@gmail.com>
2017-01-27 01:12:44 -0800

    reformating file.

--- simple_stack.py
+++ simple_stack.py
@@ -1,15 +1,17 @@
-
-
 stack = []
+
 
 def push(item):
 	stack.append(item)
 
+
 def peek():
 	return stack[len(stack) - 1]
 
+
 def pop():
 	return stack.pop()
+
 
 def print_stack():
 	print "\n\n"
@@ -18,18 +20,19 @@ def print_stack():
 
 	print "```````"
 
+
 def menu():
 	while True:
 		print "1. Push\n2. Pop\n3. Print\n4. Exit"
 		choice = input("Enter the choice: ")
 		if choice == 1:
-			item = raw_input("Enter the value: ");
+			item = raw_input("Enter the value: ")
 			push(item)
 		elif choice == 2:
 			print pop()
 		elif choice == 3:
 			print_stack()
 		elif choice == 4:
-			break;
+			break
 		else:
 			print "Enter the correct option !"
