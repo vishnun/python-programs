135cb1a30ab47172f590641eb1d78736bad18c39
Vishnu N <vishnunarang2000@gmail.com>
2017-01-27 01:10:23 -0800

    Adding basic tree operations with tests.

--- ternary_tree.py
+++ ternary_tree.py
@@ -17,18 +17,21 @@ class TernaryTree(object):
 		self.root = None
 
 	def __insert_to_left(self, node, root):
+		""" Insert the node to the left of the node. """
 		if root.left is None:
 			root.left = node
 		else:
-			self.__insert_it(root.left, node)
+			self.__insert_node(root.left, node)
 
 	def __insert_to_right(self, node, root):
+		""" Insert the node to the right of the node. """
 		if root.right is None:
 			root.right = node
 		else:
-			self.__insert_it(root.right, node)
+			self.__insert_node(root.right, node)
 
-	def __insert_it(self, root, node):
+	def __insert_node(self, root, node):
+		""" Recursively insert the node at it's correct location. """
 		if node.data == root.data:
 			while root.middle is not None:
 				root = root.middle
@@ -38,10 +41,96 @@ class TernaryTree(object):
 		elif node.data > root.data:
 			self.__insert_to_right(node, root)
 
+	def __find_smallest(self, node, parent):
+		"""Finds the smallest element in the subtree with node as the root and
+		returns the parent of as well of the smallest node."""
+		while node.left is not None:
+			parent = node
+			node = node.left
+		return node, parent
+
+	def __is_leaf_node(self, node):
+		"""Checks if the node is a leaf node with no children at all."""
+		return (node.left is None) and (node.right is None) and (node.middle is None)
+
+	def __has_left_or_right_child(self, node):
+		"""Check if the child has only left or a right child."""
+		return (node.left is None) or (node.right is None)
+
+	def __delete_if_one_child(self, node):
+		"""Delete the node when it has only one child."""
+		if node.left is not None:
+			node.data = node.left.data
+			left = node.left
+			node.left = None
+			del left
+		else:
+			node.data = node.right.data
+			right = node.right
+			node.right = None
+			del right
+
+	def __has_middle_child(self, node):
+		"""Check if a node has a middle child"""
+		return node.middle is not None
+
+	def __delete_if_left_and_right_children(self, node):
+		"""delete if a node has left and a right tree"""
+		smallest, parent = self.__find_smallest(node.right, node)
+		node.data = smallest.data
+		self.__delete_node(smallest, parent)
+
+	def __delete_leaf_node(self, node, parent):
+		"""Delete the leaf node."""
+		if parent.left is node:
+			parent.left = None
+		elif parent.right is node:
+			parent.right = None
+		del node
+
+	def __delete_node(self, node, parent):
+		""" Delete a node from the tree. Needs parent to keep it optimal. """
+		if self.__has_middle_child(node):
+			self.__delete_with_middle_child(node, parent)
+		elif self.__is_leaf_node(node):
+			self.__delete_leaf_node(node, parent)
+		elif self.__has_left_or_right_child(node):
+			self.__delete_if_one_child(node)
+		else:
+			self.__delete_if_left_and_right_children(node)
+
+	def __delete_with_middle_child(self, node, parent):
+		""" Delete a node that has one or more middle children."""
+		while node.middle is not None:
+			parent = node
+			node = node.middle
+		parent.middle = None
+		del node
+
 	def insert(self, data):
 		""" Insert a new node at it's appropriate location in the search tree. """
 		node = Node(data)
 		if self.root is None:
 			self.root = node
 		else:
-			self.__insert_it(self.root, node)
+			self.__insert_node(self.root, node)
+
+	def search(self, data):
+		""" Search for a node with the query data and return (data, parent) tuple for the node found."""
+		start = self.root
+		parent = None
+		while start is not None:
+			if data == start.data:
+				return start, parent
+			elif data < start.data:
+				parent = start
+				start = start.left
+			elif data > start.data:
+				parent = start
+				start = start.right
+		return None, None
+
+	def delete(self, data):
+		""" Delete a node from the tree. """
+		node, parent = self.search(data)
+		self.__delete_node(node, parent)
--- test_ternary_tree.py
+++ test_ternary_tree.py
@@ -1,44 +1,88 @@
-import time
-
-import ternary_tree as t_tree
-
 # Test Tree:
 #    			7 <- root
 #    		  / 7  \
 #  			 4 	7 	9
-#	 	   / 4 \  / 9 \
-#		  2    5  8   10
+#	 	   / 4 \     \
+#		  2    5      10
+
+
+import ternary_tree as t_tree
+
+
+def _insert_nodes(tree):
+	""" Helper method to create above test tree structure."""
+	tree.insert(7)
+	tree.insert(9)
+	tree.insert(4)
+	tree.insert(7)
+	tree.insert(4)
+	tree.insert(2)
+	tree.insert(10)
+	tree.insert(5)
+	tree.insert(7)
 
 
 def test_insert():
-    tree = t_tree.TernaryTree()
-    tree.insert(7)
-    tree.insert(9)
-    tree.insert(4)
-    tree.insert(7)
+	""" Tests inserting of nodes. Referes the test tree above and checks for the structure. """
+	tree = t_tree.TernaryTree()
 
-    first_left = tree.root.left
-    first_right = tree.root.right
-    first_middle = tree.root.middle
+	_insert_nodes(tree)
 
-    assert tree.root.data == 7
-    assert first_left.data == 4
-    assert first_right.data == 9
-    assert first_middle.data == 7
+	first_left = tree.root.left
+	first_right = tree.root.right
+	first_middle = tree.root.middle
 
-    tree.insert(4)
-    tree.insert(2)
-    tree.insert(4)
-    tree.insert(10)
-    tree.insert(9)
-    tree.insert(8)
-    tree.insert(5)
-    tree.insert(7)
+	assert tree.root.data == 7
+	assert first_left.data == 4
+	assert first_right.data == 9
+	assert first_middle.data == 7
+	assert first_left.left.data == 2
+	assert first_left.right.data == 5
+	assert first_left.middle.data == 4
+	assert first_right.right.data == 10
+	assert first_middle.middle.data == 7
 
-    assert first_left.left.data == 2
-    assert first_left.right.data == 5
-    assert first_left.middle.data == 4
-    assert first_right.left.data == 8
-    assert first_right.right.data == 10
-    assert first_right.middle.data == 9
-    assert first_middle.middle.data == 7
+
+def test_search():
+	""" Tests searching of a node in the tree. If found, it returns node and it's parent."""
+	tree = t_tree.TernaryTree()
+	_insert_nodes(tree)
+
+	assert tree.search(10)[0].data == 10
+	assert type(tree.search(10)[0]) == t_tree.Node
+	assert tree.search(9)[0].data == 9
+	assert type(tree.search(9)[0]) == t_tree.Node
+	assert tree.search(4)[0].data == 4
+	assert type(tree.search(4)[0]) == t_tree.Node
+	assert tree.search(1)[0] is None
+
+
+def test_delete_leaf():
+	""" Tests deletion when node has no children """
+	tree = t_tree.TernaryTree()
+	_insert_nodes(tree)
+	assert tree.search(5)[0] is not None
+	tree.delete(5)
+	assert tree.search(5)[0] is None
+
+
+def test_delete_with_one_child():
+	""" Tests deletion when a node has one child"""
+	tree = t_tree.TernaryTree()
+	_insert_nodes(tree)
+	assert tree.search(9)[0] is not None
+	tree.delete(9)
+	assert tree.search(9)[0] is None
+
+
+def test_delete_with_multiple_children():
+	""" Tests deletion of all cases with more than 1 child """
+	tree = t_tree.TernaryTree()
+	_insert_nodes(tree)
+	# When has middle child
+	assert tree.search(4)[0] is not None
+	tree.delete(4)
+	assert tree.search(4)[0] is not None
+	# when middle child is not present
+	tree.delete(4)
+	assert tree.search(4)[0] is None
